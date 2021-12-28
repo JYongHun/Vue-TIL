2a417d3a90fd417d9b5bd785ca60ddbb0b466975
YongHun <jauo40@gmail.com>
2021-12-28 15:11:15 +0900

    오타수정

--- vue2/hello-world-vite2/vite.config.js
+++ vue2/hello-world-vite2/vite.config.js
@@ -11,7 +11,7 @@
 //버전2
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from path
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
