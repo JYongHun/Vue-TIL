0189b00023bbcf9279176fda3bb46797a028a336
YongHun <jauo40@gmail.com>
2021-12-28 18:41:38 +0900

    Update VFor.vue

--- vue2/hello-world-vite2/src/components/VFor.vue
+++ vue2/hello-world-vite2/src/components/VFor.vue
@@ -12,7 +12,9 @@ import { ref } from 'vue'
 
 export default {
     setup() {
-        const items = ref (['1번 아이템', '2번 아이템', '3번 아이템'])
+        //아래와 같은 식으로도 가능 대신 순번이 낮은 순서대로 나옴
+        const items = ref ({10:'1번 아이템', 3:'2번 아이템', 9:'3번 아이템'})
+        //const items = ref ({1:'1번 아이템', 2:'2번 아이템', 4:'3번 아이템'})
         return {
             items,
         }
