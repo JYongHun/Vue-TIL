a81035bf4d6204d38721c1f0cfc7833b367aec14
YongHun <jauo40@gmail.com>
2021-12-29 18:17:24 +0900

    Todo

---
+++ vue3/todo-vite2/src/components/TodoListContainner.vue
@@ -0,0 +1,26 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { ref, readonly, provide } from 'vue'
+
+export default {
+    name: 'TodoListContainer',
+    setup() {
+        const todos = ref([])
+        provide('todos', readonly(todos))
+        const initTodos = (init_todos) => {
+            todos.vlaue = init_todos
+        }
+
+        const addTodo = (todo, date) => {}
+        const removeTodo = (id) => {}
+        const completeTodo = (id) => {}
+
+        provide('addTodo', addTodo)
+        provide('removeTodo', removeTodo)
+        provide('completeTodo', completeTodo)
+    },
+}
+</script>
