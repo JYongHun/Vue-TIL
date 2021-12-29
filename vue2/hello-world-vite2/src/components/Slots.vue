643a21b16a4a24cb3fb29adb4b284b0825ce2688
YongHun <jauo40@gmail.com>
2021-12-29 12:59:13 +0900

    Slot 데이터 전달

---
+++ vue2/hello-world-vite2/src/components/Slots.vue
@@ -0,0 +1,16 @@
+<template>
+    <ul style="list-style-type":none>
+        <li v-for="item in items">
+            <slot :item="item"></slot>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [1,2,3]
+        }
+    },
+}
+</script>
---
+++ vue2/hello-world-vite2/src/components/Slots2.vue
@@ -0,0 +1,15 @@
+<template>
+    <component>
+        <template v-slot="slotProps"> v {{slotProps.item}} </template>
+    </component>
+</template>
+
+<script>
+import Component from './Slots.vue'
+
+export default {
+    component : {
+        Component,
+    },
+}
+</script>
